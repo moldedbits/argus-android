752a78a89869a12fc278243d468b07cdbb45f507
Abhishek Bansal <discover.ab@gmail.com>
2017-06-16 17:07:58 +0530

    + sample for building a simple login screen with Argus

---
+++ email-login-sample/src/main/java/com/moldedbits/argus/samples/simplelogin/SimpleEmailLoginApplication.java
@@ -0,0 +1,38 @@
+package com.moldedbits.argus.samples.simplelogin;
+
+import android.app.Application;
+
+import com.moldedbits.argus.Argus;
+import com.moldedbits.argus.ArgusTheme;
+import com.moldedbits.argus.SimpleNextScreenProvider;
+import com.moldedbits.argus.provider.BaseProvider;
+import com.moldedbits.argus.storage.DefaultArgusStorage;
+
+import java.util.ArrayList;
+
+public class SimpleEmailLoginApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ArrayList<BaseProvider> loginProviders = new ArrayList<>();
+        loginProviders.add(new SimpleEmailLoginProvider());
+
+        // argus is customizable, supplying my custom logo to argus
+        ArgusTheme argusTheme = new ArgusTheme.Builder()
+                .logo(R.mipmap.ic_launcher_round)
+                .build();
+
+        new Argus.Builder()
+                // using default storage, dont want to do anything fancy here
+                .argusStorage(new DefaultArgusStorage(getApplicationContext()))
+                // this is the screen which should open after login is successfully completed
+                .nextScreenProvider(new SimpleNextScreenProvider(HomeActivity.class))
+                // we only want to implement login screen
+                .loginProviders(loginProviders)
+                // set custom theme
+                .theme(argusTheme)
+                .build();
+    }
+}
