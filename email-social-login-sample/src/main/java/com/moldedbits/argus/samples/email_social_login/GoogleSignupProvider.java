b91b1198787d2fed0c6fb843357f0f936bba563b
shishank <shishank.singh@moldedbits.com>
2017-09-05 17:19:08 +0530

    removed extra spaces

--- email-social-login-sample/src/main/java/com/moldedbits/argus/samples/email_social_login/GoogleSignupProvider.java
+++ email-social-login-sample/src/main/java/com/moldedbits/argus/samples/email_social_login/GoogleSignupProvider.java
@@ -3,7 +3,6 @@ package com.moldedbits.argus.samples.email_social_login;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.moldedbits.argus.provider.social.GoogleOnBoardingProvider;
 
-
 public class GoogleSignupProvider extends GoogleOnBoardingProvider {
 
     @Override
